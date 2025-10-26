0c4cf6a49eb68fb075a3fd67a16bb7e0fa6ff0e8
jefersonapps <jefersonplaynunes@gmail.com>
2025-10-26 17:24:43 -0300

    fix(layout): increase contrast in pages-quantity component

--- src/components/pages-quantity.tsx
+++ src/components/pages-quantity.tsx
@@ -7,7 +7,7 @@ interface PreviewZoomControlProps {
 export const PagesQuantity = React.memo(
   ({ pagesQuantity }: PreviewZoomControlProps) => {
     return (
-      <div className="bg-muted/50 dark:bg-muted/50 rounded-md transition-opacity border-1 border-muted shadow-sm backdrop-blur-xs absolute bottom-8 right-4 z-30 flex-shrink-0 p-2">
+      <div className="bg-muted/80 dark:bg-muted/80 rounded-md transition-opacity border-1 border-muted shadow-sm backdrop-blur-xs absolute bottom-8 right-4 z-30 flex-shrink-0 p-2">
         <div className="flex items-center gap-2 justify-center">
           <span className="text-sm font-medium w-16 text-center">
             {pagesQuantity} {pagesQuantity === 1 ? "folha" : "folhas"}
